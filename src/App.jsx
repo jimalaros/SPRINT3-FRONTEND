4336a221fdf8cc36a44da5d84b85ff6342594fbf
Jimmy Arango <jimalaros25@gmail.com>
2021-10-23 10:35:08 -0500

    Correción URL

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Auth0Provider
       domain='devstwo.us.auth0.com'
       clientId='cTzOiqZucpuhCvleQXNUqJXD7eY5jnqP'
-      redirectUri={window.location.origin}
+      redirectUri='https://frontenddevstwo.herokuapp.com/'
     >
       <div className="App">
         <Router>
